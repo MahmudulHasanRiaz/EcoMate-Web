e0ec0eb325dcb001b1f7efdfcc4693ffc35aa4fa
MahmudulHasanRiaz <mahmudriaz.bd@gmail.com>
2026-05-18 18:32:31 +0600

    feat: implement Meta and TikTok pixel tracking script initialization and queue management

--- apps/storefront/components/TrackingScripts.tsx
+++ apps/storefront/components/TrackingScripts.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import Script from "next/script";
 import { setPixelIds } from "@/lib/tracking";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:4000/api";
@@ -23,62 +22,23 @@ export default function TrackingScripts() {
       .catch(() => {});
   }, []);
 
-  // ব্রাউজারে fbq ফাংশনটি আগে থেকেই ডিফাইন করে রাখছি যেন এরর না আসে
   useEffect(() => {
-    if (typeof window !== "undefined" && !window.fbq) {
-      window.fbq = function() {
-        window.fbq.callMethod ? window.fbq.callMethod.apply(window.fbq, arguments) : window.fbq.queue.push(arguments);
-      };
-      window.fbq.queue = [];
-      window.fbq.loaded = true;
-      window.fbq.version = "2.0";
-    }
+    if (metaId || tiktokCode) {
+      // গ্লোবাল ভেরিয়েবলে আইডিগুলো রাখছি যেন এক্সটার্নাল ফাইলটি রিড করতে পারে
+      (window as any).__META_ID = metaId;
+      (window as any).__TIKTOK_CODE = tiktokCode;
 
-    if (typeof window !== "undefined" && !window.ttq) {
-      const ttq = window.ttq = [] as any;
-      ttq.methods = ["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"];
-      ttq.setAndDefer = function(t: any, e: any){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
-      for(let i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
-      window.ttq = ttq;
-    }
-  }, []);
-
-  // টিকটক লোড করার জন্য
-  useEffect(() => {
-    if (tiktokCode && typeof window !== "undefined" && window.ttq) {
-      // টিকটক নিজেই স্ক্রিপ্ট ইনজেক্ট করে
+      // এক্সটার্নাল স্ক্রিপ্ট লোড করো
       const script = document.createElement("script");
+      script.src = "/scripts/tracking.js";
       script.async = true;
-      script.src = `https://analytics.tiktok.com/i18n/pixel/events.js?sdkid=${tiktokCode}&lib=ttq`;
       document.head.appendChild(script);
-      
-      window.ttq.page();
-      
-      if (window.__flushTrackingQueue) {
-        window.__flushTrackingQueue();
-      }
-    }
-  }, [tiktokCode]);
 
-  return (
-    <>
-      {metaId && (
-        <Script
-          id="meta-pixel-src"
-          src="https://connect.facebook.net/en_US/fbevents.js"
-          strategy="afterInteractive"
-          onLoad={() => {
-            // স্ক্রিপ্ট লোড হওয়ার পর ইনিশিয়ালাইজ করো
-            window.fbq('init', metaId);
-            window.fbq('track', 'PageView');
-            
-            // কিউ ফ্লাশ করো
-            if (window.__flushTrackingQueue) {
-              window.__flushTrackingQueue();
-            }
-          }}
-        />
-      )}
-    </>
-  );
+      return () => {
+        script.remove();
+      };
+    }
+  }, [metaId, tiktokCode]);
+
+  return null;
 }
---
+++ apps/storefront/public/scripts/tracking.js
@@ -0,0 +1,45 @@
+(function() {
+  const metaId = window.__META_ID;
+  const tiktokCode = window.__TIKTOK_CODE;
+
+  // মেটা পিক্সেল
+  if (metaId && !window.fbq) {
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    
+    fbq('init', metaId);
+    fbq('track', 'PageView');
+  }
+
+  // টিকটক পিক্সেল
+  if (tiktokCode && !window.ttq) {
+    !function(w,d,t){
+      w.TiktokAnalyticsObject=t;
+      var ttq=w[t]=w[t]||[];
+      ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"];
+      ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
+      for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
+      ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e};
+      ttq.load=function(e,n){
+        var i="https://analytics.tiktok.com/i18n/pixel/events.js";
+        var o=n&&n.partner;ttq._i=ttq._i||{};ttq._i[e]=[];ttq._i[e]._u=i;
+        var r=document.createElement("script");r.type="text/javascript";r.async=true;r.src=i+"?sdkid="+e+"&lib="+t;
+        var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);
+      }; // এখানে ttq.load শেষ
+    }(window,document,'ttq'); // এখানে মূল ফাংশন শেষ এবং এক্সিকিউট হলো
+    
+    ttq.load(tiktokCode);
+    ttq.page();
+  }
+
+  // আমাদের তৈরি কিউ ফ্লাশ করো
+  if (window.__flushTrackingQueue) {
+    window.__flushTrackingQueue();
+  }
+})();
