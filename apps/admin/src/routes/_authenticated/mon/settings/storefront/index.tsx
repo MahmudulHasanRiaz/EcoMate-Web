ab1f5514bdf52d55916753a42d99f0efb1af3896
MahmudulHasanRiaz <mahmudriaz.bd@gmail.com>
2026-06-07 13:37:53 +0600

    feat(storefront-settings): wire new route, delete old monolithic file

--- apps/admin/src/routes/_authenticated/mon/settings/storefront/index.tsx
+++ apps/admin/src/routes/_authenticated/mon/settings/storefront/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { StorefrontSettings } from '@/features/settings/storefront-settings'
+import { StorefrontSettings } from '@/features/settings/storefront'
 
 export const Route = createFileRoute('/_authenticated/mon/settings/storefront/')({
   component: StorefrontSettings,
