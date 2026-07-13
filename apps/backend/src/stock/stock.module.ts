c6144bbfd7f28d3934bfca4a3b3f2a6ad900d9a9
MahmudulHasanRiaz <mahmudriaz.bd@gmail.com>
2026-07-13 12:15:25 +0600

    feat: register StockRouterService in StockModule

--- apps/backend/src/stock/stock.module.ts
+++ apps/backend/src/stock/stock.module.ts
@@ -1,9 +1,10 @@
 import { Module, Global } from '@nestjs/common';
 import { StockService } from './stock.service';
+import { StockRouterService } from './stock-router.service';
 
 @Global()
 @Module({
-  providers: [StockService],
-  exports: [StockService],
+  providers: [StockService, StockRouterService],
+  exports: [StockService, StockRouterService],
 })
 export class StockModule {}
